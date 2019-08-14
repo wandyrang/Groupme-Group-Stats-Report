2082735fea7ab5af20917f15003b9a5f9a40865a
wandyrandy <42878069+wandyrandy@users.noreply.github.com>
2019-08-13 18:48:30 -0600

    person class file and starting code for g-mo

---
+++ person.py
@@ -0,0 +1,12 @@
+'''
+A class to contain relevant data for a single member of a group on Groupme
+'''
+class Person(object):
+    def __init__(self, id, name, image_url=''):
+        self.id = id
+        self.name = name
+        self.likes_given = 0
+        self.likes_received = 0
+        self.msgs = 0
+        self.chars = 0
+        self.image_url = ''
