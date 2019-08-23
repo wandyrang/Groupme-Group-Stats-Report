32e97697bf4dccc4fbdfda41ef952507a287f437
Guillermo Rodriguez <guillermor@mail.usf.edu>
2019-08-23 00:09:41 -0600

    BabY sTePS boii

--- process_messages.py
+++ process_messages.py
@@ -1,29 +1,8 @@
 '''
-USEFUL PYTHON MODULES FOR G-MO
-[x] requests
-[x] json
-[x] csv
-
-CHECKLIST
-[x] make a groupme_person class with properties: likes_given, likes_received, #_of_messages, anything else?
-[x] call the API to get the correct group and initialize people for all the people in the group
-[]   ----> problem with Emmett where he left the group but also gave out likes, how to handle?
-[] query for messages until you reach the beginning
-[] while querying, loop through the messages and increment relevant properties on relevant messages
-[] print out the stats to a csv file to be used for data visualization
-
-
-[x]how to set the access token to an environment variable?
-[x]how to modularize searching for a group? (ask for user input?) how to modularize groupme access token? (probably not)
-
-# this is to ignore the ssl insecure warning as we are passing in 'verify=false'
-#urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
 proxy is needed if running on Intel network
 http_proxy  = "http://proxy-us.intel.com:911"
 https_proxy = "https://proxy-us.intel.com:911"
 ftp_proxy   = "ftp://proxy-us.intel.com:911"
-
 proxyDict = { 
               "http"  : http_proxy,
               "https" : https_proxy,
@@ -31,13 +10,13 @@ proxyDict = {
             }
 response = requests.get(url, verify=False, proxies=proxyDict)
 '''
-
 import json
 import requests
 import urllib3
 import os
 from person import Person
 
+TOKEN= os.environ['MY_SECRET_TOKEN']
 
 def get_group(response):
     '''
@@ -56,7 +35,7 @@ def get_group(response):
             print(i, groups["name"])
 
     # Prompt user to select which group they wanna analyze from the groups 
-    # they're in. If not in group, prompts them again.
+    # they're in. If input not in group, prompt again.
     chosen = False
     while(not chosen):
         proper_group = int(input("What group, would you like to parse? "))
@@ -79,26 +58,28 @@ def create_persons(group):
     list_of_people = []
     dict_of_people_obj = {}
 
+    # Create list of strings to pass and create People objects later
     for member in group["members"]:
         list_of_people.append(member["name"])
 
-    # For each member create a Person object
+    # For each member create a Person object and append to dict
     for person in group["members"]:
         new_person_obj = Person(person["id"], person["name"], 
                                 person["image_url"], person["nickname"], 
                                 list_of_people)
-
         dict_of_people_obj.update({new_person_obj.id: new_person_obj})
 
     return dict_of_people_obj
 
 
-def process_message(people, msg_id):
+def process_message(messages, people):
     '''
     Recieves a dict of id/people key/value pairs and a message id
-        it will call the API consecutively and return the last message_id?.. 
-        and the updated dict of people.
+        it will call the API consecutively and mutate the dict of 
+        people, incrementing the necessary fields.
     '''
+    #for msg in messages:
+        #print(json.dumps(msg, indent=2))
     
 
 def process_people(dict_people, group_id):
@@ -106,52 +87,58 @@ def process_people(dict_people, group_id):
     Recieves a dict of id/people key/value pairs and processes each message 
         updating the object and processing each message.
     '''
-    token = os.environ['MY_SECRET_TOKEN']
+    # Create URL
     limit = 100
-    url = f'https://api.groupme.com/v3/groups/{group_id}/messages?token={token}&limit={limit}'
+    url = f'https://api.groupme.com/v3/groups/{group_id}/messages?token={TOKEN}&limit={limit}'
     response = requests.get(url)
     msg_json = json.loads(response.text)
-    messages = msg_json["response"]["messages"] # Array of dicts
+    messages = msg_json["response"]["messages"] # List of dicts
     last_message_id = messages[-1]["id"]
-
-    print(json.dumps(msg_json, indent=2))
-
     count = msg_json["response"]["count"]
-    print(last_message_id)
-    print(count)
-    print("The length of the messags: ", len(msg_json["response"]["messages"]))
-    #msg_id =msg_json["response"]["messages"]["last_message_id"]
-    
 
+    print("The length of the messags: ", len(messages))
+    
+    # Loop through all of the messages in the API call
     done_processing = False
     while (not done_processing):
-        proc_people, last_message_id, count = process_message(dict_people, msg_id)
-        # Generate new msgid
-        # Generate new count
+        print("THE COUNT IS:",count)
+        if count <= 100:
+            done_processing = True
+            process_message(messages, dict_people)
+            break
 
-    if count < 100:
-        done_processing = True
+        process_message(messages, dict_people)
+
+        # Create URL again anew
+        url = f'https://api.groupme.com/v3/groups/{group_id}/messages?token='\
+              f'{TOKEN}&limit={limit}&before_id={last_message_id}'
+        print("THE FUCKKING URL IS",url)
+        response = requests.get(url)
+        msg_json = json.loads(response.text)
+        messages = msg_json["response"]["messages"] # List of dicts
+        last_message_id = messages[-1]["id"]
+        count = count - len(messages)
 
 
-    return {}
+    return dict_people
 
 
 
 def main():
     # Be sure to add the token to .env, and you're in your pipenv
-    token = os.environ['MY_SECRET_TOKEN']
-    url = 'https://api.groupme.com/v3/groups?token=' + token
+    url = f'https://api.groupme.com/v3/groups?token={TOKEN}'
     response = requests.get(url)
     response_json = json.loads(response.text)
 
-
     # Parse the json to get the group
-    group = response_json["response"][2] # Hard coded for now
-    #group = get_group(response_json)
+    #group = response_json["response"][2] # Hard coded for now
+    group = get_group(response_json)
     people = create_persons(group)
-
     proc_people = process_people(people, group["group_id"])
 
+    print("\n\nPRINTING VALUE OF PROCESSED PPL")
+    for dude in proc_people:
+        print(dude.values)
 
 
 if __name__ == '__main__':
